e0ab3d31f1e4ac20f6865359c750604c88cb3392
Steve Fagerhaug <sven@isnowhere.org>
2023-01-11 18:19:40 -0500

    server env production server.js

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: "https://strapi-on-az.azurewebsites.net",
+  admin: {
+    url: "https://purple-rock-0c3507710.2.azurestaticapps.net",
+    serveAdminPanel: false,
   },
 });
